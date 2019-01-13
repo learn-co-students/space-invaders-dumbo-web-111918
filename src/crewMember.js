840bac8748607732f0764c37487ff7ef3e74bbb2
Jesus Escalona <jesusescalona@me.com>
2019-01-13 17:39:11 -0500

    Done.

--- src/crewMember.js
+++ src/crewMember.js
@@ -1,1 +1,36 @@
-class CrewMember {}
+class CrewMember {
+
+  constructor(position, currentShip) {
+    this.position = position
+    this.currentShip = currentShip || "Looking for a Rig"
+  }
+
+  isOnShip() {
+    return this.currentShip !== "Looking for a Rig"
+  }
+
+  engageWarpDrive() {
+    if(this.isOnShip() && this.position === "Pilot") {
+      this.currentShip.warpDrive = "engaged"
+    } else {
+      return "had no effect"
+    }
+  }
+
+  setsInvisibility() {
+    if(this.isOnShip() && this.position === "Defender") {
+      this.currentShip.cloaked = true
+    } else {
+      return "had no effect"
+    }
+  }
+
+  chargePhasers() {
+    if(this.isOnShip() && this.position === "Gunner") {
+      this.currentShip.phasersCharge = "charged"
+    } else {
+      return "had no effect"
+    }
+  }
+
+}
